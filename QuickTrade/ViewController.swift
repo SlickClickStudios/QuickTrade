22cb18e7cb05da45e3535404cfef855e321c5967
Sunil Karkera <skarkera@westpac.com.au>
2020-01-27 09:38:56 +1100

    Initial commit. Setting up folder structure for QuickTrade.

---
+++ QuickTrade/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QuickTrade
+//
+//  Created by Sunil Karkera on 27/1/20.
+//  Copyright © 2020 Sunil Karkera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
